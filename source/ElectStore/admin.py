8bae98fee2ae151a53b17f87e4921237726d9f5b
ваше Yusupov-Aslan <yusupovaslan4@gmail.com>
2022-01-09 17:17:36 +0600

    Создал модель Product, сделал миграцию, так же создал urls.py и index.html добавил функцию в представлении

--- source/ElectStore/admin.py
+++ source/ElectStore/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from ElectStore.models import Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'category', 'name_goods']
+    list_filter = ['category']
+    search_fields = ['category']
+    fields = ['category', 'name_goods', 'description', 'cost', 'residue']
+    readonly_fields = ['created_at']
+
+
+admin.site.register(Product, ProductAdmin)
--- source/ElectStore/models.py
+++ source/ElectStore/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+CATEGORY_CHOICES = [("other", "Other"), ("laptops", "Laptops"), ("monitors", "Monitors"),
+                    ("office_equipment", "Office_equipment"), ("video_surveillance", "Video_surveillance")]
+
+
+class Product(models.Model):
+    name_goods = models.CharField(max_length=100, verbose_name='Наименование товара')
+    description = models.TextField(max_length=2000, null=True, blank=True, verbose_name='Текст записи')
+    category = models.CharField(max_length=20, default='other',
+                                choices=CATEGORY_CHOICES, verbose_name='Категории')
+    residue = models.PositiveIntegerField(verbose_name="Остаток")
+    cost = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Стоимость')
+    created_at = models.DateField(auto_now_add=True, verbose_name="Дата создания")
+
+    def __str__(self):
+        return f"{self.pk}. {self.name_goods}: {self.category}"
+
+    class Meta:
+        db_table = 'product'
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
