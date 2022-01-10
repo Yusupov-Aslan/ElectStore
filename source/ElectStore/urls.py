9d5aced6b7b795a57ec2a0a19bb09741bf85de7b
ваше Yusupov-Aslan <yusupovaslan4@gmail.com>
2022-01-10 11:53:26 +0600

    Создал базовый шаблон base.html, так же создан one_product.html для просмотра одного продукта! Реализовал Список товаров и просмотр одного товара по id.

--- source/ElectStore/urls.py
+++ source/ElectStore/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from ElectStore.views import index_view
+from ElectStore.views import index_view, one_product_view
 
 urlpatterns = [
     path('', index_view, name='index'),
-
+    path('product/<int:pk>/', one_product_view, name='one_product')
 ]
--- source/ElectStore/views.py
+++ source/ElectStore/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from ElectStore.models import Product
 
 
 def index_view(request):
-    return render(request, 'index.html')
+    product = Product.objects.order_by("category", "name_goods")
+    context = {'products': product}
+    return render(request, 'index.html', context)
+
+
+def one_product_view(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {"product": product}
+    return render(request, 'one_product.html', context)
